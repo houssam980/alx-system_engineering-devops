e8f4f1c7ef3e90e6f592543802da89683e1d3208
houssam980 <houssam.ghmari1243@gmail.com>
2024-03-26 14:12:44 -0700

    fixed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""get id module"""
+"""ids module"""
 
 from requests import get
 from sys import argv
@@ -11,16 +11,19 @@ headers = {
 }
 base_url = "https://jsonplaceholder.typicode.com/users/"
 
+
 def get_task_status(user_id: str) -> None:
     emp_name = get("{}{}".format(base_url, user_id)).json().get("name")
     full_url = "{}{}/todos/".format(base_url, user_id)
-    resp = get(full_url, headers=headers).json()
-    total_Tasks = len(resp)
-    done_tasks = [task['title'] for task in resp
+    response = get(full_url, headers=headers).json()
+    total_tasks = len(response)
+    done_tasks = [task['title'] for task in response
                   if task['completed']]
     done_tasks_count = len(done_tasks)
     print("Employee {} is done with tasks({}/{}):".format(
-        emp_name, done_tasks_count, total_Tasks))
+        emp_name, done_tasks_count, total_tasks))
     [print("\t {}".format(task)) for task in done_tasks]
+
+
 if __name__ == "__main__":
     get_task_status(argv[1])
