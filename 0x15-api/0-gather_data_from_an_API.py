1538bb22f6a5206b66d187bcefc28843f6919729
houssam980 <houssam.ghmari1243@gmail.com>
2024-03-26 14:06:18 -0700

    fixed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""give emplo """
+"""For a given employee IDs"""
 
 import requests
 import sys
 
 if __name__ == "__main__":
 
-    USERID = sys.argv[1]
+    userId = sys.argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(USERID))
+                        .format(userId))
+
     name = user.json().get('name')
+
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
     totalTasks = 0
     completed = 0
+
     for task in todos.json():
-        if task.get('userId') == int(USERID):
+        if task.get('userId') == int(userId):
             totalTasks += 1
             if task.get('completed'):
                 completed += 1
@@ -23,4 +26,4 @@ if __name__ == "__main__":
           .format(name, completed, totalTasks))
 
     print('\n'.join(["\t " + task.get('title') for task in todos.json()
-          if task.get('userId') == int(USERID) and task.get('completed')]))
+          if task.get('userId') == int(userId) and task.get('completed')]))
