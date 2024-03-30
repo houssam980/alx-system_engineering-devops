24cd01ae0e1dd9636428dde18d84d947e2d547c1
houssam980 <houssam.ghmari1243@gmail.com>
2024-03-30 16:05:41 +0000

    updted

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""ids module"""
 
-from requests import get
+import requests
 from sys import argv
 
-headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-    "User-Agent": "Thing Gecko/20100101 Firefox/102.0"
-}
-base_url = "https://jsonplaceholder.typicode.com/users/"
-
-
-def get_task_status(user_id: str) -> None:
-    emp_name = get("{}{}".format(base_url, user_id)).json().get("name")
-    full_url = "{}{}/todos/".format(base_url, user_id)
-    response = get(full_url, headers=headers).json()
-    total_tasks = len(response)
-    done_tasks = [task['title'] for task in response
-                  if task['completed']]
-    done_tasks_count = len(done_tasks)
-    print("Employee {} is done with tasks({}/{}):".format(
-        emp_name, done_tasks_count, total_tasks))
-    [print("\t {}".format(task)) for task in done_tasks]
-
-
 if __name__ == "__main__":
-    get_task_status(argv[1])
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            jreq = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            alltsk = len(jreq)
+            completedtsk = []
+            for t in jreq:
+                if t.get("completed") is True:
+                    completedtsk.append(t)
+            count = len(completedtsk)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, alltsk))
+            for title in completedtsk:
+                print("\t {}".format(title.get("title")))
