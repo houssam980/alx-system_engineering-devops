f44b76d805f6437877c4b2d861f93934da28c341
houssam980 <houssam.ghmari1243@gmail.com>
2024-04-10 05:15:38 -0700

    reddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+   print it
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
+    else:
+        print(None)
