6b0513335588ea150fa0085ab8651e014894df11
houssam980 <houssam.ghmari1243@gmail.com>
2024-04-10 05:14:06 -0700

    reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """
-Queries API from reddit
+queries the Reddit API
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns number of all subs"""
-    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+   return total subs
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
 
-    if response.status_code != 200:
-        return (0)
-    response = response.json()
-    if 'data' in response:
-        return (response.get('data').get('subscribers'))
-
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
     else:
-        return (0)
+        return 0
