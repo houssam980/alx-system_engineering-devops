8428e35defa3611fc0e45e883fd06c48b8619f03
houssam980 <houssam.ghmari1243@gmail.com>
2024-04-10 05:10:40 -0700

    reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,8 +5,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = ("https://api.reddit.com/r/{}/about".format(subreddit))
-    head = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, head=head, allow_redirects=False)
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return (0)
     response = response.json()
